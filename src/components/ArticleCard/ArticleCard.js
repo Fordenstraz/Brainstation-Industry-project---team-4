218a87dca669d5ddc48ac507f264eef3c84076f8
Jordan Yeung <yeungjordan@hotmail.com>
2024-08-25 11:54:32 -0400

    added json data, created article cards, fixed header and subscribed widths

---
+++ src/components/ArticleCard/ArticleCard.js
@@ -0,0 +1,28 @@
+import "./ArticleCard.scss";
+
+export default function ArticleCard(
+  props
+) {
+  return (
+    <>
+      <ul className="article-card__list">
+        {props.data.map((article) => (
+          <li
+            key={article.id}
+            className="article-card"
+          >
+            <div className="article-card__container"></div>
+            <p className="article-card__details">
+              {article.headline}
+            </p>
+            <img
+              className="article-card__img"
+              src={article.image}
+              alt={article.description}
+            ></img>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
